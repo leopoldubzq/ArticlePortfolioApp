84e79725605f03462591fa4a7d1b5592fc32389e
Leopold Romanowski <leopold.romanowski@escolasoft.com>
2024-06-05 10:33:40 +0200

    Initial Commit

---
+++ ArticlePortfolioApp/ArticlePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlePortfolioAppApp.swift
+//  ArticlePortfolioApp
+//
+//  Created by Leopold Romanowski on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ArticlePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
